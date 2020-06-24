e026c88dd32a100eb55f7cae300360538006a3c9
gregorybork <gregorybork1@gmail.com>
2020-06-24 02:43:32 -0700

    undo

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,18 +5,19 @@ class ChargesController < ApplicationController
     @amount = 500
 
     customer = Stripe::Customer.create({
-      email: params[:stripeEmail],
-      source: params[:stripeToken],
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken],
     })
 
     charge = Stripe::Charge.create({
-      customer: customer.id,
-      amount: @amount,
-      description: 'Description (pack of five stickers)',
-      currency: 'usd',
+      :customer => customer.id,
+      :amount => @amount,
+      :description => 'Description (pack of five stickers)',
+      :currency: => 'usd',
     })
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to :root
   end
+end
