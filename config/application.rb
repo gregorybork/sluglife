bd6154ba90ef36bdf0d752c8e04f74989da12170
gregorybork <gregorybork1@gmail.com>
2020-07-23 22:37:56 -0700

    carrierwave

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-AWS_ACCESS_KEY: 'AKIATJFPCEGDD2UMYT3Y'
-AWS_SECRET_KEY: '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'
-AWS_BUCKET: 'bookstore-active-storage'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +11,8 @@ module Sluger
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
