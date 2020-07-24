7838feee008a0fd2c655902cc1c34ddfbb484b38
gregorybork <gregorybork1@gmail.com>
2020-07-23 22:50:11 -0700

    carrierwave

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,9 +38,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  
+
   #for amazon s3 active storage
-  config.active_storage.service = :amazon
+  #config.active_storage.service = :amazon
 
 
   # Mount Action Cable outside main process or domain.
